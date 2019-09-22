b6f637795d886a68b45d738b93126826e5826cc3
Alisson Narjario Queiroga Diniz <alisson.diniz@venturus.org.br>
2019-09-22 19:28:14 -0300

    Updates

---
+++ precache-manifest.59ad277f229b21ad894d5c086491ef1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f7194708f3ba5ead7e0a9b5a751112",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "366515a1b8982e1cd7c5",
+    "url": "/pwa/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a558a9c88bb26ba64f22",
+    "url": "/pwa/static/js/2.b12b28ba.chunk.js"
+  },
+  {
+    "revision": "366515a1b8982e1cd7c5",
+    "url": "/pwa/static/js/main.dbd9f290.chunk.js"
+  },
+  {
+    "revision": "3a655a3de169d4ea1563",
+    "url": "/pwa/static/js/runtime-main.68341139.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/pwa/static/media/logo.25bf045c.svg"
+  }
+]);
